c0283c1fc314133958da2ca661bcafa49c526397
David <dgonzalez541@gmail.com>
2022-01-06 12:33:19 -0500

    Refactor scripts and assets into proper folder structer. Put compostie inputs and camera in proper namespaces.

---
+++ Assets/InContextLevelEditor/Scripts/Camera/FlyingCamera.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+using Assets.InContextLevelEditor;
+
+namespace InContextLevelEditor.Camera
+{
+    public class FlyingCamera : MonoBehaviour
+    {
+        public float moveSpeed;
+        public float rotateSpeed;
+
+        private AA_FlyingCamera m_Controls;
+        private Vector2 m_Rotation;
+
+        public void Awake()
+        {
+            m_Controls = new AA_FlyingCamera();
+
+            //m_Controls.Player.Elevate.started += Test;
+        }
+
+        // private void Test(InputAction.CallbackContext obj)
+        // {
+
+        // }
+
+        public void OnEnable()
+        {
+            m_Controls.Enable();
+        }
+
+        public void OnDisable()
+        {
+            m_Controls.Disable();
+        }
+
+        public void Update()
+        {
+            var look = m_Controls.Player.Look.ReadValue<Vector2>();
+            var move = m_Controls.Player.Move.ReadValue<Vector2>();
+            var elevate = m_Controls.Player.Elevate.ReadValue<float>();
+            Look(look);
+            Move(move);
+            Elevate(elevate);
+        }
+
+        private void Move(Vector2 direction)
+        {
+            if (direction.sqrMagnitude < 0.01)
+                return;
+            var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+            var move = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z) * new Vector3(direction.x, 0, direction.y);
+            transform.position += move * scaledMoveSpeed;
+        }
+
+        private void Elevate(float direction)
+        {
+            var scaledMoveSpeed = moveSpeed * Time.deltaTime;
+            var move = new Vector3(0,direction,0);
+            transform.position += move * scaledMoveSpeed;
+        }
+
+        private void Look(Vector2 rotate)
+        {
+            if (rotate.sqrMagnitude < 0.01)
+                return;
+            var scaledRotateSpeed = rotateSpeed * Time.deltaTime;
+            m_Rotation.y += rotate.x * scaledRotateSpeed;
+            m_Rotation.x = Mathf.Clamp(m_Rotation.x - rotate.y * scaledRotateSpeed, -89, 89);
+            transform.localEulerAngles = m_Rotation;
+        }
+
+        private void Fire()
+        {
+
+        }
+    }
+}
---
+++ Assets/InContextLevelEditor/Scripts/CustomInputComposites/Vector2WithOneModifier.cs
@@ -0,0 +1,47 @@
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Composites;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.Utilities;
+using UnityEditor;
+
+namespace InContextLevelEditor.CustomInputComposites
+{
+    #if UNITY_EDITOR
+    [InitializeOnLoad]
+    #endif
+    [DisplayStringFormat("{firstPart}+{secondPart}")]
+    public class Vector2WithOneModifier : InputBindingComposite<Vector2>
+    {
+        [InputControl(layout = "Vector2")]
+        public int vector2;
+    
+        [InputControl(layout = "Button")]
+        public int modifier;
+    
+    #if UNITY_EDITOR
+        static Vector2WithOneModifier()
+        {
+            Initialize();
+        }
+    #endif
+    
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Initialize()
+        {
+            InputSystem.RegisterBindingComposite<Vector2WithOneModifier>();
+        }
+    
+        public override Vector2 ReadValue(ref InputBindingCompositeContext context)
+        {
+            bool modifierValue = context.ReadValue<float>(modifier) > float.Epsilon;
+            return modifierValue ? context.ReadValue<Vector2, Vector2MagnitudeComparer>(vector2) : Vector2.zero;
+        }
+    
+        public override float EvaluateMagnitude(ref InputBindingCompositeContext context)
+        {
+            return ReadValue(ref context).magnitude;
+        }
+    }
+}
